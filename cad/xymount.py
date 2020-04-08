c0fa11273d42f3bca2b6e4e95861d1aacde9645f
bullestock <torsten@bullestock.net>
2020-04-08 19:22:18 +0200

    Get rid of magic number.

--- cad/xymount.py
+++ cad/xymount.py
@@ -37,16 +37,16 @@ def inner():
     i = right(beam_w/2)(down(1)(c2cube(iw + 2*free1 + beam_w, ih + 2*free2, overall_d+2)))
     # Screw hole
     sh = translate([0, -(ih/2 + free2 - 1), overall_d/2])(rotate([90, 0, 0])(cylinder(d = insert_d, h = th+2)))
-    return o - i + sh
+    return o - i - sh
 
 def outer():
     o = translate([beam_w/2, -beam_w/2, 0])(c2cube(inner_w + 2*free2 + 2*th, inner_h + 2*free1 + 2*th + beam_w, overall_d))
     i = translate([beam_w/2, -beam_w/2, -1])(c2cube(inner_w + 2*free2, inner_h + 2*free1 + beam_w, overall_d+2))
     sh1 = translate([-2, -(ih/2 + free2 + beam_w + th + 3), overall_d/2])(rotate([90, 0, 0])(cylinder(d = 5, h = th+2)))
     sh2 = translate([2, -(ih/2 + free2 + beam_w + th + 3), overall_d/2])(rotate([90, 0, 0])(cylinder(d = 5, h = th+2)))
-    sh = translate([27, 5, overall_d/2])(rotate([90, 0, 90])(cylinder(d = insert_d, h = th+2)))
+    sh = translate([iw/2 + free1 + beam_w + th + free2 - 1, 5, overall_d/2])(rotate([90, 0, 90])(cylinder(d = insert_d, h = th+2)))
     kh = 1
-    return o - i - hull()(sh1+sh2)- sh
+    return o - i - hull()(sh1+sh2) - sh
 
 def foot():
     y = 20
